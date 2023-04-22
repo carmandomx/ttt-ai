e6be9d3d39233c35e218e80b22cea90471c9717d
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-04-22 12:05:00 -0400

    UserS-4 completed
    Fixed an error where the CPU would make one more move after the player won.

--- src/hooks/useTikTakToe.ts
+++ src/hooks/useTikTakToe.ts
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import { TileValue } from "./../types/types";
+import React, { useState, useEffect } from "react";
 import makeCPUMove from "../utils/makeCPUMove";
 import calculateWinner from "../utils/calculateWinner";
-import { TileValue } from "../types/types";
 const useTikTakToe = () => {
   const [playerChip, setPlayerChip] = useState("");
   const [board, setBoard] = useState<TileValue[]>(Array(9).fill(null));
   const [isPlayerTurn, setIsPlayerTurn] = useState<boolean>(true);
+  const [winner, setWinner] = useState<TileValue | null>(null);
 
   const handleTileClick = (index: number): void => {
-    if (calculateWinner(board) || board[index] !== null) {
+    if (winner || board[index] !== null) {
       return;
     }
     const newBoard = [...board];
@@ -17,14 +18,24 @@ const useTikTakToe = () => {
     setIsPlayerTurn(false);
   };
 
-  if (!isPlayerTurn) {
-    makeCPUMove(board, playerChip, setBoard, setIsPlayerTurn);
-  }
+  useEffect(() => {
+    const winner = calculateWinner(board);
+    if (winner) {
+      setWinner(winner);
+      setIsPlayerTurn(false);
+    } else if (!board.includes(null)) {
+      // Game is a draw
+      setIsPlayerTurn(false);
+    } else if (!isPlayerTurn) {
+      makeCPUMove(board, playerChip, setBoard, setIsPlayerTurn);
+    }
+  }, [board, isPlayerTurn, playerChip, winner]);
 
   return {
     playerChip,
     board,
     isPlayerTurn,
+    winner,
     handleTileClick,
     setBoard,
     setIsPlayerTurn,
