dc818d388f12d0a7278f549cbc14563dc811afd1
Alexander Suarez <alexander.suarez@itksquare.edu.mx>
2023-04-24 01:33:22 -0600

    Comments added

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,27 @@ import Board from './components/Board';
 import './App.css';
 
 const App: React.FC = () => {
-  const [chip, setChip] = useState<'X' | 'O' | null>(null);
-  const [isChipSelected, setIsChipSelected] = useState<boolean>(false);
+  const [chip, setChip] = useState<'X' | 'O' | null>(null); //Use State for the chip, it can be "X", "O" or null and is initialized in null.
+  const [isChipSelected, setIsChipSelected] = useState<boolean>(false); //Use state to indicate if the user already has selected a chip, is is a boolean and it is initialized in false.
 
-  const handleChipSelection = (selectedChip: 'X' | 'O') => {
-    setChip(selectedChip);
-    setIsChipSelected(true);
+  const handleChipSelection = (selectedChip: 'X' | 'O') => { //Function to handle the click on the selecction of the chip, it has a prop that can only be a chip ("X" or "O")
+    setChip(selectedChip); //Set the chp to the one selected
+    setIsChipSelected(true); //Set to true the boolean indicating that the player has already selected a chip
   };
 
-  return (
-    <div className="App">
-      <h1>TikTakToe vs A.I.</h1>
-      {!isChipSelected && (
+  return ( //The return statement defines the JSX to render the component
+    <div className="App"> 
+      <h1>TikTakToe vs A.I.</h1> {/*Headder that will display that text*/}
+      {!isChipSelected && ( //This code block checks if isChipSelected is false. If it is, the JSX inside the parentheses is rendered. 
         <div className="chip-selection">
-          <h2>Select your chip:</h2>
-          <button onClick={() => handleChipSelection('X')}>X</button>
+          <h2>Select your chip:</h2> {/*Another heading*/}
+          {/*two buttons for selecting 'X' or 'O'. When a button is clicked, it calls the handleChipSelection function with the corresponding chip value. */}
+          <button onClick={() => handleChipSelection('X')}>X</button> 
           <button onClick={() => handleChipSelection('O')}>O</button>
         </div>
       )}
-      {isChipSelected && (
-        <Board chip={chip as 'X' | 'O'} isChipSelected={isChipSelected} setIsChipSelected={setIsChipSelected} />
+      {isChipSelected && ( //This code block checks if isChipSelected is true. If it is, the JSX inside the parentheses is rendered. 
+        <Board chip={chip as 'X' | 'O'} isChipSelected={isChipSelected} setIsChipSelected={setIsChipSelected} /> //the Board component receives the selected chip, the isChipSelected state, and the setIsChipSelected function as props.
       )}
     </div>
   );
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,53 +2,53 @@ import React from 'react';
 import { useGame } from '../hooks/useGame';
 import '../Board.css'
 
-type BoardProps = {
-  chip: 'X' | 'O';
-  isChipSelected: boolean;
-  setIsChipSelected: (value: boolean) => void;
+type BoardProps = { //Typing for the board
+  chip: 'X' | 'O'; //The chips can be "X" or "O"
+  isChipSelected: boolean; //The value of the selected chip must be a boolean
+  setIsChipSelected: (value: boolean) => void; //And the Set for chip will be a function with a bolean prop that will return a void.
 };
 
-const Board: React.FC<BoardProps> = ({ chip, isChipSelected, setIsChipSelected }) => {
-  const { board, setBoard, isPlayerTurn, setIsPlayerTurn, gameOver, gameResult, resetGame } = useGame(chip);
+const Board: React.FC<BoardProps> = ({ chip, isChipSelected, setIsChipSelected }) => { //Functional componen that will destructure the chip, the state of the chip and the function to set the chip
+  const { board, setBoard, isPlayerTurn, setIsPlayerTurn, gameOver, gameResult, resetGame } = useGame(chip); //Use of the values returned in the useGame hook.
 
-  const handleClick = (row: number, col: number) => {
-    if (board[row][col] === null && isPlayerTurn && !gameOver) {
-      const newBoard = [...board];
-      newBoard[row][col] = chip;
-      setBoard(newBoard);
-      setIsPlayerTurn(false);
+  const handleClick = (row: number, col: number) => {  //Function to set the player's chip in the board
+    if (board[row][col] === null && isPlayerTurn && !gameOver) { //If the cell is null, is the player's turn and the game isn't over.
+      const newBoard = [...board]; //Make a new board spreading the old board.
+      newBoard[row][col] = chip; //Set the cell to the player's chip
+      setBoard(newBoard); //Set the board to the new board with the player's chip
+      setIsPlayerTurn(false); //Indicate that the player's turn is over 
     }
   };
 
-  return (
+  return ( //return statement, which is responsible for rendering the JSX
     <div>
       <div className="board">
-        {board.map((row, rowIndex) => (
+        {board.map((row, rowIndex) => ( //nside the board div, the code maps over the board state (which is a 3x3 array representing the game board) and creates a new div with the class board-row for each row.
           <div key={rowIndex} className="board-row">
-            {row.map((cell, cellIndex) => (
+            {row.map((cell, cellIndex) => ( //Inside each board-row div, the code maps over each cell in the current row and creates a new div with the class board-cell.  The content of the cell is either 'X', 'O', or null (empty). 
               <div
                 key={cellIndex}
                 className="board-cell"
-                onClick={() => handleClick(rowIndex, cellIndex)}
+                onClick={() => handleClick(rowIndex, cellIndex)} //When a cell is clicked, it calls the handleClick function with the rowIndex and cellIndex as arguments.
               >
-                {cell}
+                {cell}  {/*Display of the current cell, can be "X", "O" or null */}
               </div>
             ))}
           </div>
         ))}
       </div>
-      {gameOver && (
+      {gameOver && ( //If the gameOver state is true, it means the game has ended. In this case, the code renders a div with the class result-message to display the game result, either "Draw", "Player wins" or "IA wins".
         <>
-          <div className="result-message">{gameResult}</div>
-          <div className="restart-container">
-  <button className="restart-button" onClick={() => resetGame(() => setIsChipSelected(false))}>
-    Restart
-  </button>
-</div>
+        <div className="result-message">{gameResult}</div>
+        <div className="restart-container"> {/* If the game is over, a div with the class restart-container is rendered. Inside this container, there is a "Restart" button with the class restart-button.  */}
+            <button className="restart-button" onClick={() => resetGame(() => setIsChipSelected(false))}> {/*When the button is clicked, it calls the resetGame function and resets the isChipSelected state to false. This will prompt the user to choose a chip again before starting a new game. */}
+                Restart
+            </button>
+        </div>
         </>
       )}
     </div>
   );
 };
 
-export default Board;
+export default Board; //Export of the board functional component.
