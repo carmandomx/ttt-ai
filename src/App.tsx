2df6665ff9edce9c681736515fc1ac09582ba3d3
Alexander Suarez <alexander.suarez@itksquare.edu.mx>
2023-04-23 22:10:23 -0600

    UserStory1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [chip, setChip] = useState<string | null>(null);
+
+  if (chip === null) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Tik Tak Toe vs A.I.</h1>
+          <p>Please choose your chip:</p>
+          <button onClick={() => setChip('X')}>X</button>
+          <button onClick={() => setChip('O')}>O</button>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tik Tak Toe vs A.I.</h1>
+        <p>You have chosen: {chip}</p>
       </header>
     </div>
   );
